fef603cb7d9d96076cea8e61374c35a78aab018b
dtechbro <princeakinola05@gmail.com>
2025-05-08 10:19:18 +0100

    new db update

--- db.js
+++ db.js
@@ -2,11 +2,9 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+  // If you're using SSL (recommended for production)
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
